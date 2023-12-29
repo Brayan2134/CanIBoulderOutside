2f67d894b730a707cda86b1e113423df3cc3e24b
Brayan Quevedo Ramos <brayanquevedo2134@gmail.com>
2023-12-28 19:53:18 -0800

    Search result has the updated UI!

--- lib/pages/search_result.dart
+++ lib/pages/search_result.dart
@@ -98,16 +98,34 @@ class _SearchResultPageState extends State<SearchResult> {
 
   Widget _buildRainDataDisplay() {
     if (rainData == null) {
-      return Text("Loading rain data...");
+      return const Text("Loading rain data...");
     }
     return Column(
       children: [
         Text(
-            'Last 36 hours: ${rainData!['last_36_hours']! ? 'Rained' : 'No Rain'}'),
+          'Last 36 hours: ${rainData!['last_36_hours']! ? 'Rained' : 'No Rain'}',
+          style: const TextStyle(
+            color: Colors.white,
+            fontSize: 24,
+          ),
+          textAlign: TextAlign.center,
+        ),
         Text(
-            '36 to 72 hours: ${rainData!['36_to_72_hours']! ? 'Rained' : 'No Rain'}'),
+          '36 to 72 hours: ${rainData!['36_to_72_hours']! ? 'Rained' : 'No Rain'}',
+          style: const TextStyle(
+            color: Colors.white,
+            fontSize: 24,
+          ),
+          textAlign: TextAlign.center,
+        ),
         Text(
-            'Over 72 hours: ${rainData!['over_72_hours']! ? 'Rained' : 'No Rain'}'),
+          'Over 72 hours: ${rainData!['over_72_hours']! ? 'Rained' : 'No Rain'}',
+          style: const TextStyle(
+            color: Colors.white,
+            fontSize: 24,
+          ),
+          textAlign: TextAlign.center,
+        ),
       ],
     );
   }
@@ -131,8 +149,10 @@ class _SearchResultPageState extends State<SearchResult> {
           spans.add(TextSpan(text: text.substring(lastMatchEnd, match.start)));
         }
 
-        Color color = match[0] == 'Safe:' ? safeColor
-            : match[0] == 'Caution:' ? cautionColor
+        Color color = match[0] == 'Safe:'
+            ? safeColor
+            : match[0] == 'Caution:'
+            ? cautionColor
             : dontClimbColor;
         spans.add(TextSpan(text: match[0], style: TextStyle(color: color)));
         lastMatchEnd = match.end;
@@ -149,7 +169,7 @@ class _SearchResultPageState extends State<SearchResult> {
       title: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Text(rockType),
+          Text(rockType, style: TextStyle(color: Colors.white),),
           Text(condition, style: TextStyle(color: conditionColor)),
         ],
       ),
@@ -158,7 +178,7 @@ class _SearchResultPageState extends State<SearchResult> {
           padding: const EdgeInsets.all(8.0),
           child: RichText(
             text: TextSpan(
-              style: TextStyle(color: Colors.black), // Default text style
+              style: TextStyle(color: Colors.white), // Default text style
               children: coloredTextSpans(rockInfo),
             ),
           ),
@@ -170,82 +190,184 @@ class _SearchResultPageState extends State<SearchResult> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.cityName),
-      ),
-      body: Column(
-        children: [
-          Expanded(
-            child: SingleChildScrollView(
-              child: Center(
-                child: Column(
-                  children: [
-                    Row(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.all(32.0),
-                          child: Text(_weather?.cityName ?? "Loading city..."),
-                        ),
-                        Expanded(child: Container()),
-                        Padding(
-                          padding: const EdgeInsets.all(32.0),
-                          child: Text(
-                              '${_weather?.temperature.round()}\u2109 right now'),
-                        ),
-                      ],
-                    ),
-
-                    Row(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.all(32.0),
-                          child: Column(
-                            children: [
-                              Text(
-                                  '${_weather?.tempMax.round()}\u2109 high today'),
-                              Text(
-                                  '${_weather?.tempMin.round()}\u2109 low today')
-                            ],
-                          ),
-                        ),
-                        Expanded(child: Container()),
-                        Padding(
-                          padding: const EdgeInsets.all(32.0),
-                          child: Text(
-                              '${_weather?.windSpeed.round()} mph wind speed'),
-                        ),
-                      ],
-                    ),
-
-                    _buildRainDataDisplay(),
-
-                    // Rock Conditions
-                    Row(
-                      children: [
-                        Expanded(
-                          child: Padding(
-                            padding: const EdgeInsets.all(32.0),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text("Climbing conditions"),
-                                buildClimbingConditionTile("Sandstone", "Sandstone is a sedimentary rock composed of mainly sand that absorbs moisture easily when it rains.As a result, climbing this rock type while it's still wet or damp has the potential to ruin the holds (and possibly route).Please use best judgment prior to climbing.\n\nSafe: Dry rock, no rain in over 72 hours.\nCaution: Dry rock, no rain in the last 36-72 hours.\nDo not climb: Wet rock (or/and) rain within the last 36 hours."),
-                                buildClimbingConditionTile("Conglomerate", "Conglomerate is a type of sedimentary rock that is comprised of rounded pebbles and sand. As a result, it's best to avoid climbing this rock type while the route is wet or damp. Furthermore, climbing wet routes leads to slippery and possible breaking of holds. \n\nSafe: Dry rock, no rain in the last 36-72 hours. \nCaution: Dry rock, no rain in the last 36 hours. \nDo not climb: Wet rock (and/or) rain in the last 36 hours."),
-                                buildClimbingConditionTile("Igneous", "Igneous rock is one of the three main rock types made in the earths mantle or crust. Some examples of Igneous rock include diorite, gabbro, granite, and pegmatite.They are generally safe to climb on as the rock doesn't absorb moisture well.Be advised that wet Igneous rock is moderately slippery.\n\nSafe: Dry rock, no rain in the last 36 hours.\nCaution: Wet rock, rain in the last 36 hours."),
-                                buildClimbingConditionTile("Metamorphic", "Metamorphic rock is formed when existing rocks are banded together to create a new rock. Some examples of Metamorphic rock include gneiss, quartzite, marble, and soapstone. They are generally safe to climb on as the rock doesn't absorb moisture well.\n\nSafe: Dry rock, no rain in the last 36 hours.\nCaution: Wet rock, rain in the last 36 hours."),
-                              ],
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
+      body: Stack(
+        children: <Widget>[
+          Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [
+                  Color.fromRGBO(24, 24, 24, 1),
+                  Color.fromRGBO(26, 29, 55, 1),
+                  // Replace with your desired colors
+                ],
               ),
             ),
           ),
+          Column(
+            children: <Widget>[
+              AppBar(
+                title: Text(
+                  _weather?.cityName ?? "Loading city...",
+                  style: const TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+                backgroundColor: Colors.transparent,
+                // Make the AppBar transparent.
+                elevation: 0,
+                iconTheme: const IconThemeData(
+                  color: Colors.white,
+                ),// Remove shadow.
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                  child: SingleChildScrollView(
+                    child: Column(
+                      children: [
+                        Row(
+                          children: <Widget>[
+                            Expanded(
+                              child: Container(
+                                padding: EdgeInsets.all(8),
+                                decoration: BoxDecoration(
+                                  color: Color.fromRGBO(80, 82, 94, 1),
+                                  borderRadius: BorderRadius.circular(12),
+                                ),
+                                child: Text(
+                                  '${_weather?.temperature.round()}°',
+                                  style: const TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 48,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                  textAlign: TextAlign.center,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+
+                        const SizedBox(height: 15),
+
+                        // Row to contain both FractionallySizedBoxes
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: <Widget>[
+                            Expanded(
+                              child: Container(
+                                padding: EdgeInsets.all(8),
+                                decoration: BoxDecoration(
+                                  color: Color.fromRGBO(80, 82, 94, 1),
+                                  borderRadius: BorderRadius.circular(12),
+                                ),
+                                child: Text(
+                                  '${_weather?.tempMax.round()}° High'
+                                      '\n'
+                                      '${_weather?.tempMin.round()}° Low',
+                                  style: const TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 32,
+                                  ),
+                                  textAlign: TextAlign.center,
+                                ),
+                              ),
+                            ),
+                            SizedBox(width: 16),
+                            // Spacing between the containers
+                            Expanded(
+                              child: Container(
+                                padding: EdgeInsets.all(8),
+                                decoration: BoxDecoration(
+                                  color: Color.fromRGBO(80, 82, 94, 1),
+                                  borderRadius: BorderRadius.circular(12),
+                                ),
+                                child: Text(
+                                  '${_weather?.windSpeed.round()} wind'
+                                      '\n'
+                                      '${_weather?.mainCondition}',
+                                  style: const TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 32,
+                                  ),
+                                  textAlign: TextAlign.center,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+
+                        const SizedBox(height: 15),
+
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          // Center the container in the row
+                          children: [
+                            Expanded(
+                              // Use Expanded if you want the container to take the full width
+                              child: Container(
+                                padding: EdgeInsets.all(8),
+                                // Padding inside the container
+                                decoration: BoxDecoration(
+                                  color: Color.fromRGBO(80, 82, 94, 1),
+                                  // Choose a suitable color
+                                  borderRadius: BorderRadius.circular(
+                                      12), // Rounded corners
+                                ),
+                                child:
+                                _buildRainDataDisplay(), // Your custom widget for rain data
+                              ),
+                            ),
+                          ],
+                        ),
+
+                        const SizedBox(height: 15,),
+
+                        Column(
+                          children: <Widget>[
+                            _climbingConditionsContainer(),
+                          ],
+                        ),
+
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ), // Your existing bottom navigation bar code
+    );
+  }
+
+
+  // Helper method to create a container for all climbing condition tiles
+  Widget _climbingConditionsContainer() {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 8), // Vertical spacing around the container
+      padding: EdgeInsets.all(8), // Padding inside the container
+      decoration: BoxDecoration(
+        color: Color.fromRGBO(80, 82, 94, 1), // Container color
+        borderRadius: BorderRadius.circular(12), // Rounded corners
+        // Add any other styling you need for the container
+      ),
+      child: Column(
+        children: [
+          _styledClimbingConditionTile("Sandstone", "Sandstone is a sedimentary rock composed of mainly sand that absorbs moisture easily when it rains.As a result, climbing this rock type while it's still wet or damp has the potential to ruin the holds (and possibly route).Please use best judgment prior to climbing.\n\nSafe: Dry rock, no rain in over 72 hours.\nCaution: Dry rock, no rain in the last 36-72 hours.\nDo not climb: Wet rock (or/and) rain within the last 36 hours."),
+          _styledClimbingConditionTile("Conglomerate", "Conglomerate is a type of sedimentary rock that is comprised of rounded pebbles and sand. As a result, it's best to avoid climbing this rock type while the route is wet or damp. Furthermore, climbing wet routes leads to slippery and possible breaking of holds. \n\nSafe: Dry rock, no rain in the last 36-72 hours. \nCaution: Dry rock, no rain in the last 36 hours. \nDo not climb: Wet rock (and/or) rain in the last 36 hours."),
+          _styledClimbingConditionTile("Igneous", "Igneous rock is one of the three main rock types made in the earths mantle or crust. Some examples of Igneous rock include diorite, gabbro, granite, and pegmatite.They are generally safe to climb on as the rock doesn't absorb moisture well.Be advised that wet Igneous rock is moderately slippery.\n\nSafe: Dry rock, no rain in the last 36 hours.\nCaution: Wet rock, rain in the last 36 hours."),
+          _styledClimbingConditionTile("Metamorphic", "Metamorphic rock is formed when existing rocks are banded together to create a new rock. Some examples of Metamorphic rock include gneiss, quartzite, marble, and soapstone. They are generally safe to climb on as the rock doesn't absorb moisture well.\n\nSafe: Dry rock, no rain in the last 36 hours.\nCaution: Wet rock, rain in the last 36 hours."),
         ],
       ),
     );
   }
+
+// Helper method to create a row for each climbing condition tile
+  Widget _styledClimbingConditionTile(String rockType, String rockInfo) {
+    return buildClimbingConditionTile(rockType, rockInfo); // Directly return the tile
+  }
+
 }
