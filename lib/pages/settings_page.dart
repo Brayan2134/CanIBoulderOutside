928d721fd895e62fc55bb70f97ad23901987399b
Brayan Quevedo Ramos <brayanquevedo2134@gmail.com>
2023-12-29 09:08:10 -0800

    Finished adding the settings properties -- now need to connect settings and weather_service.dart
    In this version, the settings page now responds to what units the user has inputted. For example, if the user clicks the dropdown and presses imperial, the settings page updates to reflect the changes!

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -9,7 +9,7 @@ class SettingsPage extends StatefulWidget {
 
 class _SettingsPageState extends State<SettingsPage> {
   String currentUnit = 'Metric'; // Default value
-  final List<String> units = ['Imperial', 'Metric', 'Standard'];
+  final List<String> units = ['Imperial', 'Metric'];
 
   @override
   Widget build(BuildContext context) {
@@ -43,59 +43,66 @@ class _SettingsPageState extends State<SettingsPage> {
               ),
 
               Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                    child: SingleChildScrollView(
-                      child: Column(
-                        children: [
-
-                          // Add your "Unit Type" and DropdownButton here
-                          Row(
-                            children: <Widget>[
-                              Expanded(
-                                flex: 1,
-                                child: Text(
-                                  "Unit Type",
-                                  style: TextStyle(fontSize: 16.0, color: Colors.white), // Adjust style as needed
-                                ),
-                              ),
-                              Expanded(
-                                flex: 2,
-                                child: DropdownButton<String>(
-                                  value: currentUnit,
-                                  icon: const Icon(Icons.arrow_downward, color: Colors.white),
-                                  elevation: 16,
-                                  style: const TextStyle(color: Colors.white),
-                                  underline: Container(
-                                    height: 2,
-                                    color: Colors.deepPurpleAccent,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                  child: SingleChildScrollView(
+                    child: Column(
+                      children: [
+                        // Unit Type Row
+                        settingsRow(
+                          "Unit Type",
+                          DropdownButton<String>(
+                            value: currentUnit,
+                            icon: const Icon(Icons.arrow_downward, color: Colors.white,),
+                            elevation: 16,
+                            style: const TextStyle(color: Colors.white),
+                            underline: Container(
+                              height: 2,
+                              color: Colors.deepPurpleAccent,
+                            ),
+                            onChanged: (String? newValue) {
+                              setState(() {
+                                currentUnit = newValue!;
+                              });
+                            },
+                            items: units.map<DropdownMenuItem<String>>((String value) {
+                              return DropdownMenuItem<String>(
+                                value: value,
+                                child: Container(
+                                  color: Colors.transparent,
+                                  child: Text(
+                                    value,
+                                    style: const TextStyle(
+                                      color: Colors.deepPurpleAccent,
+                                    ),
                                   ),
-                                  onChanged: (String? newValue) {
-                                    setState(() {
-                                      currentUnit = newValue!;
-                                    });
-                                  },
-                                  items: units.map<DropdownMenuItem<String>>((String value) {
-                                    return DropdownMenuItem<String>(
-                                      value: value,
-                                      child: Container(
-                                        color: Colors.transparent,
-                                        child: Text(
-                                          value,
-                                          style: const TextStyle(
-                                            color: Colors.deepPurpleAccent,
-                                          ),
-                                        ),
-                                      ),
-                                    );
-                                  }).toList(),
                                 ),
-                              ),
-                            ],
+                              );
+                            }).toList(),
                           ),
-                        ],
-                      ),
+                        ),
+
+                        // Temperature Scale Row
+                        settingsRow(
+                          "Temperature Scale",
+                          Text(
+                            getTemperatureScale(),
+                            style: const TextStyle(fontSize: 16.0, color: Colors.white),
+                          ),
+                        ),
+
+
+                        // Wind Speed Scale Row
+                        settingsRow(
+                          "Wind Speed Scale",
+                          Text(
+                            getWindSpeedScale(),
+                            style: const TextStyle(fontSize: 16.0, color: Colors.white),
+                          ),
+                        ),
+                      ],
                     ),
+                  ),
                 ),
               ),
             ],
@@ -139,4 +146,54 @@ class _SettingsPageState extends State<SettingsPage> {
   }
 
 
+  Widget settingsRow(String label, Widget settingWidget) {
+    return Container(
+      padding: const EdgeInsets.all(8), // Padding inside the container
+      margin: const EdgeInsets.only(bottom: 15), // Margin between each settings row
+      decoration: BoxDecoration(
+        color: const Color.fromRGBO(80, 82, 94, 1), // Background color for the container
+        borderRadius: BorderRadius.circular(12), // Rounded corners
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Expanded(
+            flex: 1,
+            child: Text(
+              label,
+              style: const TextStyle(fontSize: 16.0, color: Colors.white),
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: settingWidget,
+          ),
+        ],
+      ),
+    );
+  }
+
+
+
+  String getTemperatureScale() {
+    switch (currentUnit) {
+      case 'Imperial':
+        return 'Fahrenheit';
+      case 'Metric':
+        return 'Celsius';
+      default:
+        return 'Celsius'; // Default case
+    }
+  }
+
+  String getWindSpeedScale() {
+    switch (currentUnit) {
+      case 'Imperial':
+        return 'Mph';
+      case 'Metric':
+        return 'Km/hr';
+      default:
+        return 'Celsius'; // Default case
+    }
+  }
 }
